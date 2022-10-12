91cd5f46ac1d601a31c9e4921c68ce09e9c79cd4
aidan <aturner24@jcu.edu>
2022-10-12 17:16:11 -0400

    Changes: - You can now input URLs to summarize! - Minor GUI changes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 import tkinter as tk
 import openai
 from threading import *
+from bs4 import BeautifulSoup
+import requests
 
 # PUT YOUR OPEN AI KEY HERE, DELETE BEFORE COMMITTING TO GITHUB
 # This will also charge your account, be careful and use test strings if possible
@@ -19,11 +21,14 @@ frame.grid_rowconfigure(0, weight=1)
 frame.grid_columnconfigure(0, weight=1)
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
-frame.grid(row=0, column=0, sticky="N")
 
 # Initializes a variable for summarizer type
 sumType = tk.IntVar()
 sumType.set(1)
+
+# Initializes a variable for input type
+inputType = tk.IntVar()
+inputType.set(1)
 
 # Creates a thread in order to prevent window from freezing while waiting for text to generate
 def apiResponseThread():
@@ -43,14 +48,53 @@ def apiResponseThread():
 
 # This function is called to process the input of the text box in the previously mentioned thread
 def submitInput():
-    # Gets the text from the input box
+
     inp = inputBox.get(1.0, "end-1c")
 
-    if len(inp) < 50:
+    # Returns if the user inputs too little text to summarize
+    if len(inp) < 50 and inputType.get() != 2:
         outputBox.delete(1.0, END)
         outputBox.insert(INSERT, "Please enter more text")
         submitButton.config(bg="white", state="active", text="Submit")
         return
+
+    errorURLSubmit = False
+    errorMsg = ''
+
+    # Only runs when the URL option is selected
+    if inputType.get() == 2:
+        # Header that contains fake user information so request is less likely to be denied
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        # Requests URL data
+        try:
+            htmlData = requests.get(inp, headers=headers).text
+        except requests.exceptions.HTTPError:
+            errorURLSubmit = True
+            errorMsg = "HTTP Error"
+        except requests.exceptions.ConnectionError:
+            errorURLSubmit = True
+            errorMsg = "Connection Error"
+        except requests.exceptions.Timeout:
+            errorURLSubmit = True
+            errorMsg = "Timeout Error"
+        except requests.exceptions.RequestException:
+            errorURLSubmit = True
+            errorMsg = "An error has occured"
+
+        # If there was an error, it displays the error message and returns
+        if errorURLSubmit:
+            outputBox.delete(1.0, END)
+            outputBox.insert(INSERT, errorMsg)
+            submitButton.config(bg="white", state="active", text="Submit")
+            return
+
+        # Parses all <p> from the HTML document (important information is usually contained in paragraphs)
+        soupParser = BeautifulSoup(htmlData, 'html.parser')
+        data = ''
+        inp = ''
+        for data in soupParser.find_all("p"):
+            inp = inp + data.getText()
 
     # Creates the prompt for GPT-3
     prompt = ""
@@ -110,39 +154,42 @@ title = tk.Label(frame, text="Summarizer")
 title.config(font=('Helvatical bold',30))
 title.grid(row=1, column=0)
 
-# Creates subtitle
-subtitle = tk.Label(frame, text="Input text below:")
-subtitle.config(font=('Helvatical bold',15))
-subtitle.grid(row=2, column=0)
+# Creates a label for summarizer type
+typeLabel = tk.Label(frame, text="Choose input type:")
+typeLabel.grid(row=2, column=0)
+
+# Creates radio buttons for summarizer selection
+tk.Radiobutton(frame, text="Plain Text",padx = 20, variable=inputType, value=1).grid(row=3, column=0)
+tk.Radiobutton(frame, text="URL (Beta)",padx = 19, variable=inputType, value=2).grid(row=4, column=0)
 
 # Creates the textbox
-inputBox = tk.Text(frame, height=20, width=80)
-inputBox.grid(row=3, column=0)
+inputBox = tk.Text(frame, height=18, width=80)
+inputBox.grid(row=5, column=0)
 
 # Creates a label for summarizer type
 typeLabel = tk.Label(frame, text="Choose summarizer type:")
-typeLabel.grid(row=4, column=0)
+typeLabel.grid(row=6, column=0)
 
 # Creates radio buttons for summarizer selection
-tk.Radiobutton(frame, text="Bullet Points",padx = 20, variable=sumType, value=1).grid(row=5, column=0)
-tk.Radiobutton(frame, text="Paragraph    ",padx = 20, variable=sumType, value=2).grid(row=6, column=0)
+tk.Radiobutton(frame, text="Bullet Points",padx = 20, variable=sumType, value=1).grid(row=7, column=0)
+tk.Radiobutton(frame, text="Paragraph    ",padx = 20, variable=sumType, value=2).grid(row=8, column=0)
 
 # Creates submit button
 submitButton = tk.Button(frame, text="Submit", command=apiResponseThread)
-submitButton.grid(row=7, column=0)
+submitButton.grid(row=9, column=0, pady = 5)
 
 # Creates the display box for the summarized content (blank for now, it changes after it is submitted)
-outputBox = tk.Text(frame, height=20, width=80)
+outputBox = tk.Text(frame, height=18, width=80)
 outputBox.insert(INSERT, "")
-outputBox.grid(row=8, column=0)
+outputBox.grid(row=10, column=0)
 
 # Adds a scrollbar to the input and output boxes, along with the window
 scrollbarOutput = tk.Scrollbar(frame, orient='vertical', command=outputBox.yview)
-scrollbarOutput.grid(row=8, column=1, sticky=tk.NS)
+scrollbarOutput.grid(row=10, column=1, sticky=tk.NS)
 outputBox['yscrollcommand'] = scrollbarOutput.set
 scrollbarInput = tk.Scrollbar(frame, orient='vertical', command=inputBox.yview)
-scrollbarInput.grid(row=3, column=1, sticky=tk.NS)
+scrollbarInput.grid(row=5, column=1, sticky=tk.NS)
 inputBox['yscrollcommand'] = scrollbarInput.set
 
 # Starts the window
-frame.mainloop()
+window.mainloop()
